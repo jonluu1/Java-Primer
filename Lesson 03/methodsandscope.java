e8cd52b0db0fecf73ae2463361f0a6557bd517d0
Jonathan Luu <jonluu1@gmail.com>
2018-07-10 08:49:38 -0700

    methods and scope java file

---
+++ Lesson 03/methodsandscope.java
@@ -0,0 +1,13 @@
+public class methodsandscope {
+  public static void main(String[] args) {
+     printHello();
+  }
+
+  public static void printHello() {
+    System.out.println("Hello World!");
+  }
+
+  public static void printGoodbye() {
+    System.out.println("Goodbye World!");
+  }
+}
