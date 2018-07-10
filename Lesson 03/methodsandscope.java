2a2a493f1e28a23b80bae9aa36908a2ba4184991
Jonathan Luu <jonluu1@gmail.com>
2018-07-10 10:00:08 -0700

    assignment 3

--- Lesson 03/methodsandscope.java
+++ Lesson 03/methodsandscope.java
@@ -1,13 +1,21 @@
 public class methodsandscope {
   public static void main(String[] args) {
-     printHello();
+    String goodbye = "Goodbye World!";
+    printHello();
+    System.out.println(printGoodbye(goodbye));
+    printHello();
+    System.out.println(doAdd(6, 9));
   }
 
   public static void printHello() {
     System.out.println("Hello World!");
   }
 
-  public static void printGoodbye() {
-    System.out.println("Goodbye World!");
+  public static String printGoodbye(String word) {
+    return word;
+  }
+
+  public static int doAdd(int a, int b) {
+    return a + b;
   }
 }
